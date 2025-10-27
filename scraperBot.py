f491aa066e0d4c1d15b3ddee61a0da4f9b4456c9
abogaboo <abighuff@gmail.com>
2025-10-26 20:29:51 -0400

    Update scraperBot.py

--- scraperBot.py
+++ scraperBot.py
@@ -18,7 +18,7 @@ paginate_dropdown_xpath = "/html/body/div/div/div/div[2]/div/div[2]/div[2]/div[2
 
 def print_sheet(rank, data):
     for colIndex, value in enumerate(data, start = 1):
-        repo.active.cell(row=rank, column = colIndex, value=value)
+        repo.active.cell(row = rank, column = colIndex + 2, value = value)
 
 def type_in_dropdown_insights(css_selector, text):
     dropdown = driver.find_element(By.CSS_SELECTOR, css_selector)
@@ -46,25 +46,25 @@ def prep(district):
                 num_of_teams = 86
 
     # INSIGHTS PAGE
-    driver.get(insights_url) # OPENS INSIGHTS PAGE IN PRIMARY TAB
+    driver.get(insights_url) # opens insights in primary tab
     time.sleep(1) # delay for letting elements load
     type_in_dropdown_insights(district_dropdown_css, district_text)
     type_in_dropdown_insights(paginate_dropdown_css, "100")
 
     # BREAKDOWN PAGE
-    driver.execute_script("window.open('https://www.statbotics.io/teams#breakdown', '_blank');") # OPENS BREAKDOWN PAGE IN SECONDARY TAB
+    driver.execute_script("window.open('https://www.statbotics.io/teams#breakdown', '_blank');") # opens breakdown page in secondary tab
     time.sleep(1) # delay for letting elements load
-    driver.switch_to.window(driver.window_handles[1]) # SWITCH TO BREAKDOWN PAGE
+    driver.switch_to.window(driver.window_handles[1]) # switch to breakdown page
     type_in_dropdown_breakdown(district_dropdown_xpath, district_text)
     type_in_dropdown_breakdown(paginate_dropdown_xpath, "100")
     
     return num_of_teams
 
-def scrape_all(district): # 0 for peachtree, 1 for south carolina, 2 for north carolina
+def scrape_all_teams(district): # 0 for peachtree, 1 for south carolina, 2 for north carolina
     num_of_teams = prep(district)
     for i in range(1, num_of_teams + 1):
         # GETTING EPA DATA:
-        driver.switch_to.window(driver.window_handles[0]) # SWITCH TO INSIGHTS PAGE
+        driver.switch_to.window(driver.window_handles[0]) # switch to insights page
         curr_epa_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[5]/div[1]/div[1]"
         curr_auto_epa_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[6]/div[1]/div[1]"
         curr_teleop_epa_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[7]/div[1]/div[1]"
@@ -75,7 +75,7 @@ def scrape_all(district): # 0 for peachtree, 1 for south carolina, 2 for north c
         endgame_epa = driver.find_element(By.XPATH, curr_endgame_epa_path).text
 
         # GETTING ALGAE AND CORAL DATA:
-        driver.switch_to.window(driver.window_handles[1]) # SWITCH TO BREAKDOWN PAGE
+        driver.switch_to.window(driver.window_handles[1]) # switch to breakdown page
         curr_algae_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[6]/div[1]/div[1]"
         curr_l1_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[9]/div[1]/div[1]"
         curr_l2_path = f"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[{i}]/td[10]/div[1]/div[1]"
@@ -87,13 +87,18 @@ def scrape_all(district): # 0 for peachtree, 1 for south carolina, 2 for north c
         l3 = driver.find_element(By.XPATH, curr_l3_path).text
         l4 = driver.find_element(By.XPATH, curr_l4_path).text
 
-        data = [epa, auto_epa, teleop_epa, endgame_epa, algae, l1, l2, l3, l4]
-        print_sheet(i, data)
+        data = [epa, auto_epa, teleop_epa, endgame_epa, algae, l1, l2, l3, l4] # aggregate data into a list
+        print_sheet(i, data) # print list to excel sheet
         print(i, data)
-        
     repo.save("repo.xlsx")
     driver.quit()
     curr_time = str(datetime.now())
     print("Finished at " + curr_time)
 
-scrape_all(0)
+scrape_all_teams(0)
+driver = webdriver.Chrome()
+scrape_all_teams(1)
+driver = webdriver.Chrome()
+scrape_all_teams(2)
+repo.active = repo.worksheets[0]
+
